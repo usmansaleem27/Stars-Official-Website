f491e37531fb4e17659c81f2f9eaf480405c0edc
usmansaleem27 <usmansaleem2799@gmail.com>
2022-11-21 17:15:22 +0500

    Added Clock

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,26 @@ window.addEventListener("load", function(){
 $(document).ready(function () {
     $('select').niceSelect();
 });
+
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); 
+    var m = date.getMinutes(); 
+    var s = date.getSeconds(); 
+    var session = "AM";
+    
+    if(h == 0){
+     h = 12;
+    }
+    
+    if(h > 12){
+     session = "PM";
+    }
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("digital_display").innerText = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
